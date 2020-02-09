e6fb9990eda75d4c2880b0465bdfc31f55bbd890
Chitrang Natu <chit786@gmail.com>
2020-02-09 12:04:42 +0100

    feature: upgrade dependencies

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,7 +1,7 @@
 import org.junit.runner.RunWith;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
